920e9a131a85c381cfeabad62536e8f66b3125c5
Golla Rambabu <116589566+gollarambabu846@users.noreply.github.com>
2024-09-08 22:18:49 +0530

    Create Kubectl .sh

---
+++ Kubectl .sh
@@ -0,0 +1,17 @@
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/
+
+kubectl version --client --short
+
+
+eksctl Installation
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+eksctl version
